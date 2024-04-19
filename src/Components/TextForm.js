4532faa4d86bac44918293dab019b5b8231f4ae6
Gattu Nikhil Kumar <nikhilgattu16@gmail.com>
2024-04-19 22:46:15 +0530

    styles changed.

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -153,7 +153,7 @@ function ErrorDisplay(props) {
             {visible && props.error && (
                 <div className="position-relative">
                     <div className="position-absolute top-0 start-0 translate-middle p-2">
-                        <div className="alert alert-danger d-flex align-items-center" role="alert" style={{ padding: '0.5rem', fontSize: '0.875rem', left: "355px", top: "50px" }}>
+                        <div className="alert alert-danger d-flex align-items-center" role="alert" style={{ padding: '0.5rem', fontSize: '0.875rem', left: "270px", top: "50px" }}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" width="16"
                                 height="16" role="img" aria-label="Warning:">
                                 <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
